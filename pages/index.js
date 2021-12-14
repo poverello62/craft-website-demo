9952b6675427befd5a22d6ed3789f0fc6f9a1fda
poverello62 <82802800+poverello62@users.noreply.github.com>
2021-12-14 14:13:18 +0800

    Update index.js
    Poverello

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>My Digital Garden!</title>
+        <title>Poverello's Digital Garden!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
